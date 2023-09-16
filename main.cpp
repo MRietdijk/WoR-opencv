23c9cccc37c83ede21953bef6c9f0ad9160d7c04
Mart Rietdijk <mart.rietdijk@gmail.com>
2023-09-16 20:32:51 +0200

    removed main comments

--- main.cpp
+++ main.cpp
@@ -5,59 +5,8 @@
 #include "Input.h"
 
 int main(int argc, char *argv[])
-{   
-    // std::string feedChoice(argv[1]);
-    // std::string file(argv[2]);
-
-    // std::cout << argv[1] << std::endl;
-    // if (argc != 3) {
-    //     std::cout << "not enough parameters" << std::endl;
-    //     return -1;
-    // }
-
-    // std::unique_ptr<Feed> feed;
-    
-    // if (feedChoice == "camera") {
-    //     std::cout << argv[2] << std::endl;
-    //     feed = std::make_unique<CameraFeed>(file);
-    // }
-    // else if (feedChoice == "file") {
-    //     feed = std::make_unique<FileFeed>(file);
-    // }
-
-    // feed->showSliders();
-
-    // while (true)
-    // {
-    //     cv::Mat img = feed->getFeed();
-    //     cv::imshow("Feed", img);
-    //     img = feed->processImg(img, true);
-    //     cv::waitKey(1);
-    // }
-    
-
-    // cv::destroyAllWindows();
-
-
-
+{  
     Input input(argc, argv);
-
     input.handleInput();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     return 0;
 }
